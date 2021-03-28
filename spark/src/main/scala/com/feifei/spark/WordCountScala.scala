ab43a9bb8a3a69e7ffd93df1e1315f6d55930e13
程飞 <chengfei@newland.com.cn>
2021-03-28 10:26:45 +0800

    SparkWordCount源码分析

--- spark/src/main/scala/com/feifei/spark/WordCountScala.scala
+++ spark/src/main/scala/com/feifei/spark/WordCountScala.scala
@@ -8,19 +8,32 @@ object WordCountScala {
     val conf = new SparkConf()
     conf.setAppName("wordcount")
     conf.setMaster("local")
+//    conf.set("spark.default.parallelism","2")
     val sc = new SparkContext(conf)
+    sc.setLogLevel("error")
 
+
+//    sc.textFile("./data/testdata.txt")
+//      .flatMap(_.split(" "))
+//      .map((_,1))
+//      .reduceByKey(_+_)
+//      .foreach(println)
+//
+//    sc.textFile("./data/testdata.txt")
+//      .flatMap(_.split(" "))
+//      .map((_,1))
+//      .reduceByKey(_+_)
+//      .foreach(println)
+
+    //textFile()最多只能设置两个平行度
     val fileRDD: RDD[String] = sc.textFile("data/testdata.txt")
     val flatMapRDD: RDD[String] = fileRDD.flatMap(_.split(" "))
     val mapRDD: RDD[(String, Int)] = flatMapRDD.map((_,1))
     val reduceRDD: RDD[(String, Int)] = mapRDD.reduceByKey(_+_)
     reduceRDD.foreach(println)
 
-    sc.textFile("./data/testdata.txt")
-      .flatMap(_.split(" "))
-      .map((_,1))
-      .reduceByKey(_+_)
-      .foreach(println)
+
+
     while (true){}
   }
 }
